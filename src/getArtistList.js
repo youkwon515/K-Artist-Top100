4088ba9f14f2dd7570562850f9a2327be2895d72
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-09-07 20:41:45 +0900

    FEAT : getArtistList 생성

---
+++ src/getArtistList.js
@@ -0,0 +1,55 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+
+async function getHtml() {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://charts.youtube.com/charts/TopArtists/kr?hl=ko');
+        await page.waitForSelector('.chart-table');
+        const html = await page.content();
+        await browser.close();
+        return html;
+    } catch(error) {
+        console.error(error);
+        return null;
+    }
+}
+
+async function getArtistList() {
+    try {
+        const html = await getHtml();
+        if (!html) return;
+
+        const artistList = [];
+        const $ = cheerio.load(html);
+
+        $(".chart-table-row").each((idx, element) => {
+            let rank_change = 'no';
+
+            switch($(element).find("path.style-scope").attr('d')) {
+                case 'M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z':
+                    rank_change = 'no';
+                    break;
+                case 'M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z':
+                    rank_change = 'up';
+                    break;
+                case 'M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z':
+                    rank_change = 'down';
+                    break;
+            }
+
+            artistList.push({
+                rank: idx + 1,
+                rank_change,
+                artist: $(element).find(".ytmc-ellipsis-text-container span.ytmc-ellipsis-text").text().replace(/\s/g, ""),
+            });
+        });
+        
+        return artistList;
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+module.exports = (getArtistList);
