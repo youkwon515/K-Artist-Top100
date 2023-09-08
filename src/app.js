73ac8c34c19729f28f82e4af81e5caa939e05b86
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-09-08 17:46:40 +0900

    BUILD : react 빌드

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 const getArtistList = require('./getArtistList');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../artist-chart/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../artist-chart/build/index.html'));
+});
 
 app.get('/api/artistList', async (req, res) => {
     try {
