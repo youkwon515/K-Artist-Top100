7b968abb8c08b54733c5507667caa1e68c7f4db3
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-09-08 16:01:24 +0900

    FIX : ChartItem 수정

--- artist-chart/src/components/ChartItem/index.tsx
+++ artist-chart/src/components/ChartItem/index.tsx
@@ -1,6 +1,16 @@
-function ChartItem() {
+import * as S from './styled';
+
+import Item from '../../types/ChartItem';
+
+function ChartItem({rank, artist, rankChange, duration, views}: Item) {
     return (
-        <div>chart</div>
+        <S.ItemContainer>
+            <S.Rank>{rank}</S.Rank>
+            <S.Artist>{artist}</S.Artist>
+            <S.RankChange>{rankChange}</S.RankChange>
+            <S.Duration>{duration}</S.Duration>
+            <S.Views>{views}</S.Views>
+        </S.ItemContainer>
     );
 }
 
---
+++ artist-chart/src/components/ChartItem/styled.ts
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    display : flex;
+    width : 1000px;
+    height : 190px;
+    margin : 0 auto;
+    margin-bottom : 100px;
+    background : #E5E4E2;
+    border-radius : 5px 5px 10px 10px;
+    filter: drop-shadow(0 0 10px #E5E4E2);
+`;
+
+export const Rank = styled.div`
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    width : 15%;
+    height : 100%;
+    font-size : 70px;
+    font-family: 'Kanit', sans-serif;
+`;
+
+export const Artist = styled.div`
+    display : flex;
+    justify-content: flex-start;
+    align-items: center;
+    width : 55%;
+    height : 100%;
+    font-size : 37px;
+    font-family: 'Do Hyeon', sans-serif;
+`;
+
+export const RankChange = styled.div`
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    width : 10%;
+    height : 100%;
+    font-size : 30px;
+    font-family: 'Kanit', sans-serif;
+`;
+
+export const Duration = styled.div`
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    width : 10%;
+    height : 100%;
+    font-size : 30px;
+    font-family: 'Kanit', sans-serif;
+`;
+
+export const Views = styled.div`
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    width : 10%;
+    height : 100%;
+    font-size : 30px;
+    font-family: 'Kanit', sans-serif;
+`;
